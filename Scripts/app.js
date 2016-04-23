749b0305db48f3b117d335b829c39e2ed47004b8
Dan <dchen12812@gmail.com>
2016-04-22 22:12:14 -0700

    init current progress

---
+++ Scripts/app.js
@@ -0,0 +1,27 @@
+/*
+	D3 Visualization of Protein domains from Uniprot
+*/
+
+//Setup width of chart and bar height
+var width = 420,
+    barHeight = 20;
+
+//Create scale for the chart
+var x = d3.scale.linear()
+    .range([0, width]);
+
+//Create the svg .chart element in #chart-div with width of width
+var chart = d3.select("#chart-div").append("svg").classed("chart", true).attr("width", width);
+
+//Read domain data
+d3.csv('Input/domain_data.csv')
+  .row(function (d) { return d.GENENAME })
+  .get(function (error, rows) {
+    
+    //Create the options for the datalist
+    d3.select('datalist').selectAll('option')
+      .data(rows) // Data join
+      .enter() // Enter data selection
+      .append('option') // Append to options
+      .attr('value', function (d) { return d; }); // Add name to option
+  });
